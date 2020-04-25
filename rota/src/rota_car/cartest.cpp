a16fb8af285435e04c1d74cbfacb39c98cb8add0
MadMcCrow <https://MadMcCrow@github.com>
2020-04-25 19:02:49 +0200

    update rota folder to gdnative test

---
+++ rota/src/rota_car/cartest.cpp
@@ -0,0 +1,27 @@
+#include "cartest.hpp"
+
+using namespace godot;
+
+void GDRotaCarTest::_register_methods() {
+    register_method("_process", &GDRotaCarTest::_process);
+}
+
+GDRotaCarTest::GDRotaCarTest() {
+}
+
+GDRotaCarTest::~GDRotaCarTest() {
+    // add your cleanup here
+}
+
+void GDRotaCarTest::_init() {
+    // initialize any variables here
+    time_passed = 0.0;
+}
+
+void GDRotaCarTest::_process(float delta) {
+    time_passed += delta;
+
+    Vector2 new_position = Vector2(10.0 + (10.0 * sin(time_passed * 2.0)), 10.0 + (10.0 * cos(time_passed * 1.5)));
+
+    set_position(new_position);
+}
